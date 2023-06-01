2a3b5ecdc8bd8d002bfb7053636e5e7df44909bd
Facundo Damián Fagnano <facundofagnano@gmail.com>
2023-06-01 01:23:19 -0300

    Teste Servicios Login & Register

---
+++ src/test/java/ar/edu/unlam/tallerweb1/domain/usuarios/ServicioLoginTest.java
@@ -0,0 +1,53 @@
+package ar.edu.unlam.tallerweb1.domain.usuarios;
+
+import ar.edu.unlam.tallerweb1.infrastructure.RepositorioUsuario;
+import org.junit.Before;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class ServicioLoginTest {
+
+    private RepositorioUsuario repositorioUsuario;
+    private HttpServletRequest request;
+    private HttpSession sesion;
+
+    private ServicioLoginImpl servicioLogin;
+
+    @Before
+    public void init() {
+        repositorioUsuario = mock(RepositorioUsuario.class);
+        sesion = mock(HttpSession.class);
+        request = mock(HttpServletRequest.class);
+        servicioLogin = new ServicioLoginImpl(this.repositorioUsuario);
+    }
+
+    @Test
+    public void consultarUsuario(){
+        String mail = "facundo@mail.com";
+        String password = "hola1234";
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("facundo@mail.com");
+        usuario.setPassword("hola1234");
+
+        when(repositorioUsuario.buscarUsuario(any(),any())).thenReturn(usuario);
+        Usuario resultUser = servicioLogin.consultarUsuario(mail, password);
+
+        assertThat(resultUser).isNotNull();
+        assertThat(resultUser.getEmail()).isNotNull();
+        assertThat(resultUser.getEmail()).isNotEmpty();
+        assertThat(resultUser.getEmail()).isEqualTo(mail);
+        assertThat(resultUser.getPassword()).isNotNull();
+        assertThat(resultUser.getPassword()).isNotEmpty();
+        assertThat(resultUser.getPassword()).isEqualTo(password);
+    }
+
+}
---
+++ src/test/java/ar/edu/unlam/tallerweb1/domain/usuarios/ServicioRegisterTest.java
@@ -0,0 +1,60 @@
+package ar.edu.unlam.tallerweb1.domain.usuarios;
+
+import ar.edu.unlam.tallerweb1.infrastructure.RepositorioUsuario;
+import org.junit.Before;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+
+public class ServicioRegisterTest {
+
+    private RepositorioUsuario repositorioUsuario;
+    private HttpServletRequest request;
+    private HttpSession sesion;
+    private ServicioRegisterImpl servicioRegister;
+
+    @Before
+    public void init() {
+        repositorioUsuario = mock(RepositorioUsuario.class);
+        sesion = mock(HttpSession.class);
+        request = mock(HttpServletRequest.class);
+        servicioRegister = new ServicioRegisterImpl(this.repositorioUsuario);
+    }
+
+    @Test
+    public void registrarUsuario(){
+        String mail = "facundo@mail.com";
+        String password = "hola1234";
+        String rol = "alumno";
+
+        doNothing().when(repositorioUsuario).registrar(any(),any(),any());
+
+        servicioRegister.registrarUsuario(mail, password, rol);
+
+        verify(repositorioUsuario, times(1)).registrar(any(),any(),any());
+
+    }
+
+    @Test
+    public void consultarUsuario(){
+        String mail = "facundo@mail.com";
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("facundo@mail.com");
+
+        when(repositorioUsuario.buscarMail(any())).thenReturn(usuario);
+        Usuario resultUser = servicioRegister.consultarUsuario(mail);
+
+        assertThat(resultUser).isNotNull();
+        assertThat(resultUser.getEmail()).isNotNull();
+        assertThat(resultUser.getEmail()).isNotEmpty();
+        assertThat(resultUser.getEmail()).isEqualTo(mail);
+    }
+
+}
