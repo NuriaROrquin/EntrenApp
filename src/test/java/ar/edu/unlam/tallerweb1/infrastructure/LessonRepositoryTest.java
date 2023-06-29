5c09b0f8f5a097f6c159b0493d42feb971e40fa0
sanopera <91997129+sanopera@users.noreply.github.com>
2023-06-28 22:25:51 -0300

    Update LessonRepositoryTest.java

--- src/test/java/ar/edu/unlam/tallerweb1/infrastructure/LessonRepositoryTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/infrastructure/LessonRepositoryTest.java
@@ -199,4 +199,89 @@ public class LessonRepositoryTest extends SpringTest {
 
     }
 
+    /*@Test
+    @Transactional
+    @Rollback
+    public void whenINeedToKnowAllTheAvailableClassesToSingInAsStudent() {
+
+        //rol Alumno
+        Rol rolAlumno = new Rol();
+        rolAlumno.setIdRole(2);
+
+        session().save(rolAlumno);
+
+        //alumno
+        Usuario alumno = new Usuario();
+        alumno.setId(2L);
+        alumno.setRol(rolAlumno);
+        alumno.setName("Pablo");
+
+        session().save(alumno);
+
+        Rol role = new Rol();
+        role.setDescription("professor");
+        role.setIdRole(3L);
+
+        Usuario professor = new Usuario();
+        professor.setId(1L);
+        professor.setEmail("pabloantunez@mail.com");
+        professor.setRol(role);
+        professor.setPassword("1234");
+
+        // Lugar
+        BasicData dataPlace = new BasicData();
+        Lugar place = dataPlace.createPlace(1L,34615743L, 58503336L, "Un lugar unico","Club Buenos Aires");
+        session().save(place);
+
+        // Dificultad
+        BasicData dataDifficulty = new BasicData();
+        Dificultad difficulty = dataDifficulty.createDifficulty(1L, "Avanzado");
+        session().save(difficulty);
+
+        // Disciplina
+        BasicData dataDiscipline = new BasicData();
+        Disciplina discipline = dataDiscipline.createDiscipline(1L,"Crossfit", "Entrena tu cuerpo al maximo", 18, 40);
+        session().save(discipline);
+
+
+        // Detalle
+        BasicData dataDetail = new BasicData();
+        BasicData detailStartHour = new BasicData();
+        BasicData detailEndHour = new BasicData();
+        LocalTime startTime = detailStartHour.setHourMinutes(2,30);
+        LocalTime endTime = detailEndHour.setHourMinutes(4,00);
+        Detalle detail = dataDetail.createDetail(1L,startTime,endTime,50 );
+        session().save(detail);
+
+        // Clase 1
+        BasicData dataLesson = new BasicData();
+        Clase lesson = dataLesson.createClase(1,new Date(2023,12,30), new Date(2023,10,20),new Date(2024,12,31), detail, place, difficulty, discipline, professor);
+        session().save(lesson);
+
+        // Clase 2
+        BasicData dataLesson2 = new BasicData();
+        Clase lesson2 = dataLesson2.createClase(1,new Date(2023,11,10), new Date(2023,11,10),new Date(2024,05,30), detail, place, difficulty, discipline, professor);
+        session().save(lesson2);
+
+        List<Clase> expectingLessons = new ArrayList<>();
+        expectingLessons.add(lesson);
+        expectingLessons.add(lesson2);
+
+        CriteriaBuilder criteriaBuilder = session().getCriteriaBuilder();
+        CriteriaQuery<Clase> criteriaQuery = criteriaBuilder.createQuery(Clase.class);
+        Root<Clase> usuarioClaseRoot = criteriaQuery.from(Clase.class);
+        Join<Clase, AlumnoClase> claseJoin = usuarioClaseRoot.join("lesson",JoinType.INNER);
+        //Join<AlumnoClase, Usuario> alumnoJoin = usuarioClaseRoot.join("user");
+        //Join<Clase, Usuario> profesorJoin = claseJoin.join("professor");
+
+        criteriaQuery.select(usuarioClaseRoot).where(criteriaBuilder.equal(claseJoin.get("user"),alumno));
+
+        List<Clase> lessons = session().createQuery(criteriaQuery).getResultList();
+
+        assertThat(lessons).isNotEmpty();
+        //assertThat(lessons).isNotNull();*/
+
+
+}
+
 }
